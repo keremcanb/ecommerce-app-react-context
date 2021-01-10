2ad1d168e0ccdd1ab1f8d43eebb1f866f81a948b
keremcanb <keremcan@gmail.com>
2021-01-11 01:15:49 +0300

    sort functionality

--- src/context/actions/filter_context.js
+++ src/context/actions/filter_context.js
@@ -16,6 +16,7 @@ const initialState = {
   filtered_products: [],
   all_products: [],
   grid_view: false,
+  sort: 'name-a',
 };
 
 const FilterContext = createContext();
@@ -28,15 +29,28 @@ export const FilterProvider = ({ children }) => {
     dispatch({ type: LOAD_PRODUCTS, payload: products });
   }, [products]);
 
+  useEffect(() => {
+    dispatch({ type: SORT_PRODUCTS });
+  }, [products, state.sort]);
+
+  // Set view
   const setGridView = () => {
     dispatch({ type: SET_GRID_VIEW });
   };
-
   const setListView = () => {
     dispatch({ type: SET_LIST_VIEW });
   };
+  // Sort
+  const updateSort = (e) => {
+    const { value } = e.target;
+    dispatch({ type: UPDATE_SORT, payload: value });
+  };
 
-  return <FilterContext.Provider value={{ ...state, setGridView, setListView }}>{children}</FilterContext.Provider>;
+  return (
+    <FilterContext.Provider value={{ ...state, setGridView, setListView, updateSort }}>
+      {children}
+    </FilterContext.Provider>
+  );
 };
 
 export const useFilterContext = () => {
--- src/context/reducers/filter_reducer.js
+++ src/context/reducers/filter_reducer.js
@@ -15,12 +15,35 @@ const filter_reducer = (state, action) => {
   switch (type) {
     case LOAD_PRODUCTS:
       return { ...state, all_products: [...payload], filtered_products: [...payload] };
-
+    // Set view
     case SET_LIST_VIEW:
       return { ...state, grid_view: false };
-
     case SET_GRID_VIEW:
       return { ...state, grid_view: true };
+    // Sort products
+    case UPDATE_SORT:
+      return { ...state, sort: payload };
+    case SORT_PRODUCTS: {
+      const { sort, filtered_products } = state;
+      const tempProducts = [...filtered_products];
+      if (sort === 'price-lowest') {
+        tempProducts.sort((a, b) => a.price - b.price);
+      }
+      if (sort === 'price-highest') {
+        tempProducts.sort((a, b) => b.price - a.price);
+      }
+      if (sort === 'name-a') {
+        tempProducts.sort((a, b) => {
+          return a.name.localeCompare(b.name);
+        });
+      }
+      if (sort === 'name-z') {
+        tempProducts.sort((a, b) => {
+          return b.name.localeCompare(a.name);
+        });
+      }
+      return { ...state, filtered_products: tempProducts };
+    }
 
     default:
       return state;
