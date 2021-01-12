b14ffac73b61231b48027d193ee8d9a84827c2c6
keremcanb <keremcan@gmail.com>
2021-01-12 22:47:37 +0300

    m

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # eCommerce App React Context
 
-Part of Gatsby Tutorial and Projects Course by John Smilga
+An eCommerce with advanced filtering system and cart functionality.
+
+Libraries used for the app are Styled componenets, Axios, React Router, Auth0
+
+Part of React Tutorials and Projects Course by John Smilga
--- src/context/reducers/cart_reducer.js
+++ src/context/reducers/cart_reducer.js
@@ -3,28 +3,33 @@ import { ADD_TO_CART, CLEAR_CART, COUNT_CART_TOTALS, REMOVE_CART_ITEM, TOGGLE_CA
 const cart_reducer = (state, action) => {
   const { type, payload } = action;
   const { cart } = state;
-  const { id, color, amount, product } = payload;
+  const {
+    id,
+    color,
+    amount,
+    product: { name, images, price, stock }
+  } = payload;
 
   switch (type) {
     case ADD_TO_CART: {
       const item = cart.find((i) => i.id === id + color);
       if (item) {
-        console.log();
+        //
       } else {
         const newItem = {
           id: id + color,
-          name: product.name,
+          name,
           color,
           amount,
           image: product.images[0].url,
-          price: product.price,
-          max: product.stock
+          price,
+          max: stock
         };
         return { ...state, cart: [...cart, newItem] };
       }
-      return { ...state, sort: payload };
     }
 
+    // eslint-disable-next-line no-fallthrough
     default:
       return state;
       throw new Error(`No Matching "${action.type}" - action type`);
